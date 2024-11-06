8908ccdfdabef2c430eab69ca1654debd09e87c7
JBlitzar <90664831+JBlitzar@users.noreply.github.com>
2024-11-05 16:42:07 -0800

    Update script.js

--- exoplanet/script.js
+++ exoplanet/script.js
@@ -1,105 +1,122 @@
 var data = [];
-var useLog = false
+var useLog = false;
 var simulation;
 function getRandomItems(array, numItems) {
-    const shuffled = array.sort(() => 0.5 - Math.random());
-    return shuffled.slice(0, numItems);
+  const shuffled = array.sort(() => 0.5 - Math.random());
+  return shuffled.slice(0, numItems);
 }
-function $(s){
-    var a = document.querySelectorAll(s);
-    return a.length > 1 ? a : a[0];
+function $(s) {
+  var a = document.querySelectorAll(s);
+  return a.length > 1 ? a : a[0];
 }
-$("#log").addEventListener('change', function(){
-   useLog = $("#log").checked;
-   simulation
-   .force("collide", d3.forceCollide().radius(d => getR(d)+1).iterations(3))
-   .alpha(1)
-   .restart()
-})
+$("#log").addEventListener("change", function () {
+  useLog = $("#log").checked;
+  simulation
+    .force(
+      "collide",
+      d3
+        .forceCollide()
+        .radius((d) => getR(d) + 1)
+        .iterations(3)
+    )
+    .alpha(1)
+    .restart();
+});
 
-function getR(d){
-    r = Math.abs(d.r) + 1
-    return useLog ? Math.log(r) :  r
+function getR(d) {
+  r = Math.abs(d.r) + 1;
+  return useLog ? Math.log(r) : r;
 }
-const tooltip = d3.select("body") // selects body
-    .append("div") // adds div
-    .attr("class", "label") // sets options for div
-    .style("visibility", "hidden")
-    .text("");
+const normalizeRValues = (objects) =>
+  objects.map(
+    ({ r }) =>
+      ((r - Math.min(...objects.map((o) => o.r))) /
+        (Math.max(...objects.map((o) => o.r)) -
+          Math.min(...objects.map((o) => o.r)))) *
+      200
+  );
+
+const tooltip = d3
+  .select("body") // selects body
+  .append("div") // adds div
+  .attr("class", "label") // sets options for div
+  .style("visibility", "hidden")
+  .text("");
 d3.csv("data.csv").then((d) => {
-    d.forEach((point) => {
-        if (point["pl_rade"]) {
-            data.push({
-                r: point["pl_rade"],
-                group: 1,
-                name: point["pl_name"]
-            });
-        }
-    });
-
-    console.log(data);
-    data = getRandomItems(data,200).map(Object.create);
-
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-
-    const svg = d3.select("svg")
-        .attr("width", width)
-        .attr("height", height);
-
-    const color = d3.scaleOrdinal(d3.schemeTableau10);
-
-    const nodes = data.map(Object.create);
-
-    simulation = d3.forceSimulation(nodes)
-        .alphaTarget(0.3)
-        .velocityDecay(0.1)
-        .force("x", d3.forceX().strength(0.01))
-        .force("y", d3.forceY().strength(0.01))
-        .force("collide", d3.forceCollide().radius(d => getR(d)+1).iterations(3))
-        .force("charge", d3.forceManyBody().strength((d, i) => i ? 0 : 0))//-width * 2 / 3))
-        .on("tick", ticked);
-
-
-    svg.on("pointermove", pointermoved);
-
-    function pointermoved(event) {
-        const [x, y] = d3.pointer(event);
-        // nodes[0].fx = x - width / 2;
-        // nodes[0].fy = y - height / 2;
+  d.forEach((point) => {
+    if (point["pl_rade"]) {
+      data.push({
+        r: point["pl_rade"],
+        group: 1,
+        name: point["pl_name"],
+      });
     }
-    
-   
+  });
 
-    const circles = svg.selectAll("circle")
-        .data(nodes)
-        .enter().append("circle")
-        .attr("r", d => d.r)
-        .attr("fill", d => color(d.group));
+  console.log(data);
+  data = getRandomItems(data, 200).map(Object.create);
 
-    function ticked() {
-        circles
-            .attr("cx", d => width / 2 + d.x)
-            .attr("cy", d => height / 2 + d.y)
-            .attr("r", d => getR(d) )
-            .on("mouseover", function(event, d) {
-                tooltip.style("visibility", "visible").text(d["name"] + "");
-            })
-            .on("mousemove", function(event) {
-                tooltip.style("top", (event.pageY - 10) + "px")
-                       .style("left", (event.pageX + 10) + "px");
-            })
-            .on("mouseout", function() {
-                tooltip.style("visibility", "hidden");
-            });
+  const width = window.innerWidth;
+  const height = window.innerHeight;
 
-    }
+  const svg = d3.select("svg").attr("width", width).attr("height", height);
 
-    window.addEventListener("unload", () => simulation.stop());
+  const color = d3.scaleOrdinal(d3.schemeTableau10);
 
-    
+  const nodes = data.map(Object.create);
 
+  simulation = d3
+    .forceSimulation(nodes)
+    .alphaTarget(0.3)
+    .velocityDecay(0.1)
+    .force("x", d3.forceX().strength(0.01))
+    .force("y", d3.forceY().strength(0.01))
+    .force(
+      "collide",
+      d3
+        .forceCollide()
+        .radius((d) => getR(d) + 1)
+        .iterations(3)
+    )
+    .force(
+      "charge",
+      d3.forceManyBody().strength((d, i) => (i ? 0 : 0))
+    ) //-width * 2 / 3))
+    .on("tick", ticked);
 
+  svg.on("pointermove", pointermoved);
 
+  function pointermoved(event) {
+    const [x, y] = d3.pointer(event);
+    // nodes[0].fx = x - width / 2;
+    // nodes[0].fy = y - height / 2;
+  }
 
+  const circles = svg
+    .selectAll("circle")
+    .data(nodes)
+    .enter()
+    .append("circle")
+    .attr("r", (d) => d.r)
+    .attr("fill", (d) => color(d.group));
+
+  function ticked() {
+    circles
+      .attr("cx", (d) => width / 2 + d.x)
+      .attr("cy", (d) => height / 2 + d.y)
+      .attr("r", (d) => getR(d))
+      .on("mouseover", function (event, d) {
+        tooltip.style("visibility", "visible").text(d["name"] + "");
+      })
+      .on("mousemove", function (event) {
+        tooltip
+          .style("top", event.pageY - 10 + "px")
+          .style("left", event.pageX + 10 + "px");
+      })
+      .on("mouseout", function () {
+        tooltip.style("visibility", "hidden");
+      });
+  }
+
+  window.addEventListener("unload", () => simulation.stop());
 });
